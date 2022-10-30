cb0385cb7712e93e8b24f1e8c2564c4e7b414e87
Florian Tünte <boba2002fett@gmail.com>
2022-10-30 17:48:55 +0100

    compiles

--- src/convert.rs
+++ src/convert.rs
@@ -1,4 +1,5 @@
 use std::{path::PathBuf, thread::JoinHandle};
+use futures::StreamExt;
 use tokio::{io::copy, task::JoinError};
 use tempfile::Builder;
 
@@ -12,13 +13,13 @@ pub async fn process_job(job_id: String) -> () {
     
     let tmp_dir = Builder::new().prefix(&job_id).tempdir().unwrap();
 
-    // let source_files = futures::stream::iter(source_files)
-    // .map(|source_file| {
-    //     let path = tmp_dir.path().join(&source_file.source_file_id);
-    //     async move {
-    //         dowload_source_file(ref_client, &source_file.source_uri, path).await
-    //     }
-    // }).buffer_unordered(10).collect::<Vec<PathBuf>>().await;
+    let source_files = futures::stream::iter(job_model.source_files)
+    .map(|source_file| {
+        let path = tmp_dir.path().join(&source_file.source_file_id);
+        async move {
+            dowload_source_file(ref_client, &source_file.source_uri, path).await
+        }
+    }).buffer_unordered(10).collect::<Vec<PathBuf>>().await;
     // let source_file_handles = source_files.iter().map(|source_file| {
     //     let path = tmp_dir.path().join(&source_file.source_file_id);
     //     tokio::spawn(async {
@@ -27,11 +28,11 @@ pub async fn process_job(job_id: String) -> () {
     // }).collect::<Vec<tokio::task::JoinHandle<PathBuf>>>();
     // let source_file_results: Vec<Result<PathBuf, JoinError>> = futures::future::join_all(source_file_handles).await;
     // let source_files: Vec<PathBuf> = source_file_results.into_iter().flatten().collect();
-    let mut source_files: Vec<PathBuf> = Vec::with_capacity(job_model.source_files.len());
-    for source_file in job_model.source_files {
-        let path = tmp_dir.path().join(&source_file.source_file_id);
-        source_files.push(dowload_source_file(ref_client, &source_file.source_uri, path).await);
-    }
+    // let mut source_files: Vec<PathBuf> = Vec::with_capacity(job_model.source_files.len());
+    // for source_file in job_model.source_files {
+    //     let path = tmp_dir.path().join(&source_file.source_file_id);
+    //     source_files.push(dowload_source_file(ref_client, &source_file.source_uri, path).await);
+    // }
 
     let pdfium = get_pdfium();
 
@@ -41,7 +42,7 @@ pub async fn process_job(job_id: String) -> () {
             let source_path = source_files.iter().find(|path| path.ends_with(&part.source_file_id)).unwrap();
             let source_doc = pdfium.load_pdf_from_file(source_path, None).unwrap();
             if source_doc.pages().len() <= part.start_page_number.unwrap_or_else(|| u16::MAX) && source_doc.pages().len() <= part.end_page_number.unwrap_or_else(|| u16::MAX) {
-                _ = set_error(&job_id).await;
+                // _ = set_error(&job_id).await;
                 return ()
             }
             add_page(&mut new_doc, source_doc, &part);
