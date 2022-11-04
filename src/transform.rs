bb45172730a03d485d888c6000dd3f3a6c857224
Florian Tünte <boba2002fett@gmail.com>
2022-11-04 22:40:39 +0100

    Refacture transform

--- src/transform.rs
+++ src/transform.rs
@@ -8,60 +8,43 @@ pub fn get_pdfium() -> Pdfium
             .or_else(|_| Pdfium::bind_to_system_library()).unwrap())
 }
 
-pub fn add_page(new_document: &mut PdfDocument, source_document: &mut PdfDocument, part: &Part) {
-    match (part.start_page_number, part.end_page_number) {
-        (None, None) => {
-            new_document.pages().append(&source_document).unwrap()
-        },
-        (None, Some(end_page_number)) => {
-            new_document.pages()
-                .copy_page_range_from_document(&source_document,
-                    0..=end_page_number - 1,
-                    new_document.pages().len()
-                ).unwrap()
-        },
-        (Some(start_page_number), None) => {
-            new_document.pages()
-                .copy_page_range_from_document(&source_document,
-                    start_page_number - 1..=source_document.pages().len() - 1,
-                    new_document.pages().len()
-                ).unwrap()
-        },
-        (Some(start_page_number), Some(end_page_number)) => {
-            if start_page_number == end_page_number && part.rotation.is_some() {
-                {
-                    let pages = source_document.pages();
-                    let mut page = pages.iter().nth((start_page_number - 1).into()).unwrap();
-                    let rotation = page.rotation().unwrap();
-                    let part_rotation: i32 = part.rotation.as_ref().unwrap_or(&Rotation::P0).as_degrees();
-                    let turn_rotation: i32 = {
-                        if part_rotation < 0 {
-                            360 + part_rotation
-                        }
-                        else {
-                            part_rotation
-                        }
-                    };
-                    let mut new_rotation = rotation.as_degrees() as i32 + turn_rotation;
-                    if new_rotation > 360 {
-                        new_rotation -= 360;
-                    }
-                    let new_rotation = match new_rotation {
-                        0 => PdfBitmapRotation::None,
-                        90 => PdfBitmapRotation::Degrees90,
-                        180 => PdfBitmapRotation::Degrees180,
-                        270 => PdfBitmapRotation::Degrees270,
-                        _ => PdfBitmapRotation::None,
-                    };
-                    page.set_rotation(new_rotation);
-                }
-                new_document.pages().copy_page_from_document(&source_document, start_page_number - 1, new_document.pages().len());
+pub fn add_page(new_document: &mut PdfDocument, source_document: &mut PdfDocument, part: &Part) -> Result<(), PdfiumError> {
+    let start_page_number = part.start_page_number.unwrap_or(1);
+    let end_page_number = part.end_page_number.unwrap_or(source_document.pages().len());
+    turn_single_page(start_page_number, end_page_number, source_document, part);
+    new_document.pages()
+    .copy_page_range_from_document(&source_document,
+        start_page_number - 1..=end_page_number - 1,
+        new_document.pages().len()
+    )?;
+    Ok(())
+}
+
+fn turn_single_page(start_page_number: u16, end_page_number: u16, source_document: &mut PdfDocument, part: &Part) {
+    if start_page_number == end_page_number && part.rotation.is_some() {
+        let pages = source_document.pages();
+        let mut page = pages.iter().nth((start_page_number - 1).into()).unwrap();
+        let rotation = page.rotation().unwrap();
+        let part_rotation: i32 = part.rotation.as_ref().unwrap_or(&Rotation::P0).as_degrees();
+        let turn_rotation: i32 = {
+            if part_rotation < 0 {
+                360 + part_rotation
             }
-            new_document.pages()
-                .copy_page_range_from_document(&source_document,
-                    start_page_number - 1..=end_page_number - 1,
-                    new_document.pages().len()
-                ).unwrap()
-        },
+            else {
+                part_rotation
+            }
+        };
+        let mut new_rotation = rotation.as_degrees() as i32 + turn_rotation;
+        if new_rotation > 360 {
+            new_rotation -= 360;
+        }
+        let new_rotation = match new_rotation {
+            0 => PdfBitmapRotation::None,
+            90 => PdfBitmapRotation::Degrees90,
+            180 => PdfBitmapRotation::Degrees180,
+            270 => PdfBitmapRotation::Degrees270,
+            _ => PdfBitmapRotation::None,
+        };
+        page.set_rotation(new_rotation);
     }
 }
