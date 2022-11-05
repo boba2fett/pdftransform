ca1230fcd0b8c48f36b3069742d91d1fd5c48c60
Florian Tünte <boba2002fett@gmail.com>
2022-11-05 14:54:09 +0100

    More error handling

--- src/convert.rs
+++ src/convert.rs
@@ -12,31 +12,39 @@ pub async fn process_job(job_id: String) -> () {
         
         let job_files = get_job_files(&job_id).await;
 
-        let source_files = futures::stream::iter(job_model.source_files)
+        let source_files: Vec<Result<PathBuf, &'static str>> = futures::stream::iter(job_model.source_files)
         .map(|source_file| {
             let path = job_files.get_path(&source_file.source_file_id);
             async move {
                 dowload_source_file(ref_client, &source_file.source_uri, path).await
             }
-        }).buffer_unordered(10).collect::<Vec<PathBuf>>().await;
+        }).buffer_unordered(10).collect::<Vec<Result<PathBuf, &'static str>>>().await;
 
-        let results: Result<_, &str> = process(&job_id, job_model.documents, source_files, job_files);
-        _ = match results {
-            Ok(results) => set_ready(&job_id, results).await,
-            Err(err) => set_error(&job_id, err).await,
-        };
+        let failed = source_files.iter().find(|source_file| source_file.is_err());
+
+        if failed.is_none() {
+            let source_files = source_files.iter().map(|source_file| source_file.as_ref().unwrap()).collect();
+            let results: Result<_, &str> = process(&job_id, job_model.documents, source_files, job_files);
+            _ = match results {
+                Ok(results) => set_ready(&job_id, results).await,
+                Err(err) => set_error(&job_id, err).await,
+            };
+        }
+        else {
+            _ = set_error(&job_id, failed.as_ref().unwrap().as_ref().err().unwrap()).await;
+        }
     }
 }
 
-fn process(job_id: &String, documents: Vec<Document>, source_files: Vec<PathBuf>, job_files: JobFileProvider) -> Result<Vec<DocumentResult>, &'static str> {
+fn process(job_id: &String, documents: Vec<Document>, source_files: Vec<&PathBuf>, job_files: JobFileProvider) -> Result<Vec<DocumentResult>, &'static str> {
     {
         let mut results = Vec::with_capacity(documents.len());
         for document in documents {
             let pdfium = init_pdfium();
-            let mut new_doc = pdfium.create_new_pdf().map_err(|_| "Could not create document")?;
+            let mut new_doc = pdfium.create_new_pdf().map_err(|_| "Could not create document.")?;
             for part in document.binaries {
                 let source_path = source_files.iter().find(|path| path.ends_with(&part.source_file_id)).ok_or("Could not find corresponding source file.")?;
-                let mut source_doc = pdfium.load_pdf_from_file(source_path, None).map_err(|_| "Could not create document")?;
+                let mut source_doc = pdfium.load_pdf_from_file(source_path, None).map_err(|_| "Could not create document.")?;
                 add_page(&mut new_doc, &mut source_doc, &part).map_err(|_| "Error while converting, were the page numbers correct?")?;
             }
             let path = job_files.get_path(&document.id);
@@ -51,11 +59,11 @@ fn process(job_id: &String, documents: Vec<Document>, source_files: Vec<PathBuf>
     }
 }
 
-async fn dowload_source_file(client: &reqwest::Client, source_file_url: &str, path: PathBuf) -> PathBuf {
-    let mut response = client.get(source_file_url).send().await.unwrap();
-    let mut file = tokio::fs::File::create(&path).await.unwrap();
-    while let Some(mut item) = response.chunk().await.unwrap() {
-        file.write_all_buf(&mut item).await.unwrap();
+async fn dowload_source_file(client: &reqwest::Client, source_file_url: &str, path: PathBuf) -> Result<PathBuf, &'static str> {
+    let mut response = client.get(source_file_url).send().await.map_err(|_| "Could not download document.")?;
+    let mut file = tokio::fs::File::create(&path).await.map_err(|_| "Could not create file.")?;
+    while let Some(mut item) = response.chunk().await.map_err(|_| "Could read response.")? {
+        file.write_all_buf(&mut item).await.map_err(|_| "Could write to file.")?;
     }
-    path
+    Ok(path)
 }
