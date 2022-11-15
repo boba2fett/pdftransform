08d01c73b304b91f47113d2ea5a9e98e6aad36b8
Florian Tünte <boba2002fett@gmail.com>
2022-11-15 22:53:36 +0100

    Repair expire mechanism

--- src/files.rs
+++ src/files.rs
@@ -7,12 +7,19 @@ use rocket::fs::FileName;
 use tokio::fs;
 use mongodb_gridfs::{options::GridFSBucketOptions, GridFSBucket};
 
-use crate::{persistence::{get_job_model, generate_30_alphanumeric}, consts};
+use crate::{persistence::{get_job_model, generate_30_alphanumeric}, consts::{self}};
+
+const FILES_COLLECTION: &str = "fs.files";
+const CHUNKS_COLLECTION: &str = "fs.chunks";
+
+fn get_bucket(client: &mongodb::Client) -> GridFSBucket {
+    let db = client.database(consts::NAME);
+    GridFSBucket::new(db, Some(GridFSBucketOptions::default()))
+}
 
 pub async fn get_job_result_file(client: &mongodb::Client, job_id: &str, token: &str, file_id: &str) -> Result<impl Stream<Item = Vec<u8>>, &'static str> {
     _ = get_job_model(client, job_id, token).await?;
-    let db = client.database(consts::NAME);
-    let bucket = GridFSBucket::new(db, Some(GridFSBucketOptions::default()));
+    let bucket = get_bucket(client);
     if let Ok(id) = ObjectId::from_str(file_id) {
         let cursor = bucket.open_download_stream(id).await.map_err(|_| "Could not find file.")?;
         return Ok(cursor)
@@ -21,11 +28,10 @@ pub async fn get_job_result_file(client: &mongodb::Client, job_id: &str, token: 
 }
 
 pub async fn store_job_result_file(client: &mongodb::Client, file_name: &str, source: impl AsyncRead + Unpin) -> Result<String, &'static str> {
-    let db = client.database(consts::NAME);
-    let mut bucket = GridFSBucket::new(db, Some(GridFSBucketOptions::default()));
+    let mut bucket = get_bucket(client);
     let file_id = bucket.upload_from_stream(file_name, source, None).await.map_err(|_| "Could not store result.").map(|id| id.to_string())?;
-    let chunks = client.database(consts::NAME).collection::<()>("chunks");
-    let result = chunks.update_many(doc!{ "files_id": &file_id }, doc!{"$set": {"uploadDate": DateTime::now()}} , None).await;
+    let chunks = client.database(consts::NAME).collection::<()>(CHUNKS_COLLECTION);
+    let result = chunks.update_many(doc!{ "files_id": ObjectId::from_str(&file_id).unwrap() }, doc!{"$set": {"uploadDate": DateTime::now()}} , None).await;
     if result.is_err() {
         warn!("Could not set uploadDate for chunks of {}.", &file_id, {fileId: &file_id});
     }
@@ -33,8 +39,8 @@ pub async fn store_job_result_file(client: &mongodb::Client, file_name: &str, so
 }
 
 pub async fn set_expire_after(client: &mongodb::Client, seconds: u64) -> Result<(), Error> {
-    let files = client.database(consts::NAME).collection::<()>("files");
-    let chunks = client.database(consts::NAME).collection::<()>("chunks");
+    let files = client.database(consts::NAME).collection::<()>(FILES_COLLECTION);
+    let chunks = client.database(consts::NAME).collection::<()>(CHUNKS_COLLECTION);
 
     let options = IndexOptions::builder().expire_after(Duration::new(seconds, 0)).build();
     let index = IndexModel::builder()
