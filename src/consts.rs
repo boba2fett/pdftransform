58b9aaa3ebf8ee1f4baa773bfba22901cd74c1bb
Florian Tünte <boba2002fett@gmail.com>
2022-12-16 21:03:36 +0100

    Use a nicer number

--- src/consts.rs
+++ src/consts.rs
@@ -1,4 +1,4 @@
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const NAME: &str = env!("CARGO_PKG_NAME");
 pub static mut PARALLELISM: usize = 10;
-pub static mut MAX_KIBIBYTES: usize = 4048;
+pub static mut MAX_KIBIBYTES: usize = 4096;
